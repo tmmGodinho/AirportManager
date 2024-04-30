759e7b491842cfa1e0a1991a01af154c6e2e4a92
tmmGodinho <tomas.mmg@gmail.com>
2024-04-30 22:16:23 +0100

    cleaned TODOs + AirportUtils

--- src/main/java/com/airportmanager/planemanager/Controller.java
+++ src/main/java/com/airportmanager/planemanager/Controller.java
@@ -5,7 +5,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+//import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
